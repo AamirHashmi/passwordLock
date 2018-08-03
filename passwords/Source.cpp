4827380c9ef4c93c226c2adcf4a54c86fd3cf26c
Aamir Hashmi <aamiruhashmiru@gmail.com>
2018-08-03 20:23:03 +0100

    remove my actual password haha

--- passwords/Source.cpp
+++ passwords/Source.cpp
@@ -8,14 +8,14 @@ using namespace std;
 int main() {
 	
 
-
+	string password = "password"; //my password.
 	int count = 0;
 	bool gainedAccess = false;
 	bool isLocked = false;
 	string attempt;
 	string locked;
 
-	ifstream fin;
+	ifstream fin;                                     //openin up a text file to check if the password program is locked.
 	fin.open("c:\\meta_verse\\locked.txt", ios::in);
 	fin >> locked;
 	fin.close();
@@ -28,16 +28,16 @@ int main() {
 		while (!gainedAccess && count != 3) {
 			cout << "Enter the password" << endl;
 			cin >> attempt;
-			if (attempt == "HelloDaVe123") {
+			if (attempt == password) {
 				gainedAccess = true;
-				ShellExecute(0, "open", "c:\\meta_verse\\od.txt", NULL, NULL, 1);
+				ShellExecute(0, "open", "c:\\meta_verse\\od.txt", NULL, NULL, 1);  //opens passwords if attempt matches password.
 			}
 			else {
 				cout << "Wrong password try again" << endl;
 			}
 			count++;
 		}
-		if (!gainedAccess) {
+		if (!gainedAccess) {                         //if the user makes 3 wrong attempts, the program is closed and locked.
 			cout << "Too many wrong attempts" << endl;
 			ofstream fout;
 			fout.open("c:\\meta_verse\\locked.txt", ios::out);
